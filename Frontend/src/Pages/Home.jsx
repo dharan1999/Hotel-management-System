8679be1b80ece2c6616612855316d3aa2ba1c7ad
dharan1999 <dharanthakar96@gmail.com>
2024-06-17 10:30:54 -0400

    Started Frontend

---
+++ Frontend/src/Pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hero from "../Components/Hero";
+import Biography from "../Components/Biography";
+import MessageForm from "../Components/MessageForm";
+import Departments from "../Components/Departments";
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero
+        title={
+          "Welcome to ZeeCare Medical Institute | Your Trusted Healthcare Provider"
+        }
+        imageUrl={"/hero.png"}
+      />
+      <Biography imageUrl={"/about.png"} />
+      <Departments />
+      <MessageForm />
+    </>
+  )
+}
+
+export default Home
